a9c22dd7db625d7c0d9f1ef5f5589fdde9807eab
radekwarowny <radekwarownydev@gmail.com>
2021-07-29 13:34:46 +0100

    refresh

--- ocr.py
+++ ocr.py
@@ -7,12 +7,18 @@ import pytesseract
 
 def ocr_core(filename):
     """ OCR processing of images. """
-    #pytesseract.pytesseract.tesseract_cmd = '/app/vendor/tesseract-ocr/bin/tesseract'
+
     # Using Pillow to open image and pytesseract to read string in the image
     text = pytesseract.image_to_string(Image.open(filename))
     # print(filename)
 
     return text
 
-# print(ocr_core(noisy.png))
+# print(ocr_core('noisy.png'))
+
+# print(ocr_core('asda.png'))
+
+# print(ocr_core('Tesco.jpg'))
+
+
 
